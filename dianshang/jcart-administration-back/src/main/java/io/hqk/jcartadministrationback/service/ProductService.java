a270bc61d1ff62351f50aebec647abcd25f48282
huangqinkai <2725926259@qq.com>
2020-03-09 19:11:56 +0800

    add administration product seach with filter

--- dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/service/ProductService.java
+++ dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/service/ProductService.java
@@ -2,6 +2,7 @@ package io.hqk.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
 import io.hqk.jcartadministrationback.dto.in.ProductCreateInDTO;
+import io.hqk.jcartadministrationback.dto.in.ProductSearchInDTO;
 import io.hqk.jcartadministrationback.dto.in.ProductUpdateInDTO;
 import io.hqk.jcartadministrationback.dto.out.ProductListOutDTO;
 import io.hqk.jcartadministrationback.dto.out.ProductShowOutDTO;
@@ -18,7 +19,8 @@ public interface ProductService {
 
     void batchDelete(List<Integer> productIds);
 
-    Page<ProductListOutDTO> search(Integer pageNum);
+    Page<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
+                                   Integer pageNum);
 
     ProductShowOutDTO getById(Integer productId);
 
--- dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/service/impl/ProductServiceImpl.java
+++ dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/service/impl/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import com.github.pagehelper.PageHelper;
 import io.hqk.jcartadministrationback.dao.ProductDetailMapper;
 import io.hqk.jcartadministrationback.dao.ProductMapper;
 import io.hqk.jcartadministrationback.dto.in.ProductCreateInDTO;
+import io.hqk.jcartadministrationback.dto.in.ProductSearchInDTO;
 import io.hqk.jcartadministrationback.dto.in.ProductUpdateInDTO;
 import io.hqk.jcartadministrationback.dto.out.ProductListOutDTO;
 import io.hqk.jcartadministrationback.dto.out.ProductShowOutDTO;
@@ -97,9 +98,9 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Page<ProductListOutDTO> search(Integer pageNum) {
+    public Page<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO, Integer pageNum) {
         PageHelper.startPage(pageNum, 10);
-        Page<ProductListOutDTO> page = productMapper.search();
+        Page<ProductListOutDTO> page = productMapper.search(productSearchInDTO.getProductCode(),productSearchInDTO.getStatus(),productSearchInDTO.getStockQuantity());
         return page;
     }
 
