3ba2119e0d0a54162db27a29bc7007a9abdc701a
huangqinkai <2725926259@qq.com>
2020-03-10 19:39:01 +0800

    add administration order seach api

--- dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/dao/OrderMapper.java
+++ dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/dao/OrderMapper.java
@@ -3,7 +3,10 @@ package io.hqk.jcartadministrationback.dao;
 import com.github.pagehelper.Page;
 import io.hqk.jcartadministrationback.dto.out.OrderListOutDTO;
 import io.hqk.jcartadministrationback.po.Order;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository
 public interface OrderMapper {
@@ -21,6 +24,11 @@ public interface OrderMapper {
 
 //    custom
 
-    Page<OrderListOutDTO> search();
+    Page<OrderListOutDTO> search(@Param("orderId") Long orderId,
+                                 @Param("status")Byte status,
+                                 @Param("totalpric") Double totalpric,
+                                 @Param("customerName") String customerName,
+                                 @Param("startTime") Date startTime,
+                                 @Param("endTime") Date endTime);
 
 }
--- dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/service/OrderService.java
+++ dianshang/jcart-administration-back/src/main/java/io/hqk/jcartadministrationback/service/OrderService.java
@@ -1,13 +1,14 @@
 package io.hqk.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import io.hqk.jcartadministrationback.dto.in.OrderSearchInDTO;
 import io.hqk.jcartadministrationback.dto.out.OrderListOutDTO;
 import io.hqk.jcartadministrationback.dto.out.OrderShowOutDTO;
 import io.hqk.jcartadministrationback.po.Order;
 
 public interface OrderService {
 
-    Page<OrderListOutDTO> search(Integer pageNum);
+    Page<OrderListOutDTO> search(OrderSearchInDTO orderSearchInDTO,Integer pageNum);
 
     OrderShowOutDTO getById(Long orderId);
 
