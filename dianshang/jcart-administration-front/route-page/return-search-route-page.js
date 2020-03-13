f7a0842aa8cad8ee7c0d52407dc0f0f7eaac5eb2
huangqinkai <2725926259@qq.com>
2020-03-13 20:34:16 +0800

    fix page

--- dianshang/jcart-administration-front/route-page/return-search-route-page.js
+++ dianshang/jcart-administration-front/route-page/return-search-route-page.js
@@ -1,10 +1,12 @@
-const RoutePage = {
+const ReturnSearchRoutePage = {
     template: `
     <div id="app">
+
+        <el-input v-model="returnId" placeholder="请输入退货Id"></el-input>
+        <el-input v-model="orderId" placeholder="请输入订单Id"></el-input>
+        <el-input v-model="customerName" placeholder="请输入客户姓名"></el-input>
         <el-input v-model="productCode" placeholder="请输入商品代号"></el-input>
         <el-input v-model="productName" placeholder="请输入商品名称"></el-input>
-        <el-input v-model="price" placeholder="请输入价格"></el-input>
-        <el-input v-model="stockQuantity" placeholder="请输入库存"></el-input>
         <br>
 
         <el-select v-model="selectedStatus" placeholder="请选择状态">
@@ -12,38 +14,42 @@ const RoutePage = {
             </el-option>
         </el-select>
         <br>
+
+        <el-date-picker v-model="startTime" type="date" placeholder="选择开始日期">
+        </el-date-picker>
+        <br>
+
+        <el-date-picker v-model="endTime" type="date" placeholder="选择结束日期">
+        </el-date-picker>
+        <br>
+
         <el-button type="primary" @click="handleSearchClick">搜索</el-button>
         <el-button type="primary" @click="handleClearClick">清空条件</el-button>
 
         <el-table :data="pageInfo.list" style="width: 100%">
-            <el-table-column label="主图">
-                <template slot-scope="scope">
-                    <el-image style="width: 100px; height: 100px" :src="scope.row.mainPicUrl" fit="fill"></el-image>
-                </template>
+            <el-table-column prop="returnId" label="退货Id">
             </el-table-column>
-            <el-table-column prop="productCode" label="商品代码">
+            <el-table-column prop="orderId" label="订单Id">
+            </el-table-column>
+            <el-table-column prop="customerName" label="客户姓名">
+            </el-table-column>
+            <el-table-column prop="productCode" label="商品代号">
             </el-table-column>
             <el-table-column prop="productName" label="商品名称">
             </el-table-column>
-            <el-table-column label="价格">
-                <template slot-scope="scope">
-                    <s>{{scope.row.price}}</s> <br>
-                    {{(scope.row.price * scope.row.discount).toFixed(2)}}
-                </template>
-            </el-table-column>
-            <!-- <el-table-column prop="discount" label="打折">
-            </el-table-column> -->
-            <el-table-column prop="stockQuantity" label="库存">
-            </el-table-column>
-            <el-table-column label="状态">
+            <el-table-column prop="status" label="状态">
                 <template slot-scope="scope">
                     {{statuses[scope.row.status].label}}
                 </template>
             </el-table-column>
-            <el-table-column label="操作">
+            <el-table-column label="申请日期">
                 <template slot-scope="scope">
-                    <router-link :to="'/product/update/'+scope.row.productId">编辑</router-link>
-                    <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    {{(new Date(scope.row.createTimestamp)).toLocaleString()}}
+                </template>
+            </el-table-column>
+            <el-table-column label="修改日期">
+                <template slot-scope="scope">
+                    {{(new Date(scope.row.updateTimestamp)).toLocaleString()}}
                 </template>
             </el-table-column>
         </el-table>
@@ -55,54 +61,61 @@ const RoutePage = {
     data() {
         return {
             pageInfo: '',
-            pageNum: 1,
+            returnId: '',
+            orderId: '',
+            customerName: '',
             productCode: '',
             productName: '',
-            price: '',
-            stockQuantity: '',
             selectedStatus: '',
             statuses: [
-                { value: 0, label: '下架' },
-                { value: 1, label: '上架' },
-                { value: 2, label: '待审核' }
-            ]
+                { value: 0, label: '待处理' },
+                { value: 1, label: '待取货' },
+                { value: 2, label: '正在处理' },
+                { value: 3, label: '完成' },
+                { value: 4, label: '拒绝' }
+            ],
+            startTime: '',
+            endTime: '',
+            pageNum: 1
         }
     },
     mounted() {
         console.log('view mounted');
-        this.searchProduct();
+        this.searchReturn();
     },
     methods: {
         handleSearchClick() {
             console.log('search click');
             this.pageNum = 1;
-            this.searchProduct();
-        },
-        handleEdit(index, row) {
-            console.log('product edit click', index, row);
-            this.$router.push('/product/update/' + row.productId);
+            this.searchReturn();
         },
         handleClearClick() {
             console.log('clear click');
+            this.returnId = '';
+            this.orderId = '';
+            this.customerName = '';
             this.productCode = '';
             this.productName = '';
-            this.price = '';
-            this.stockQuantity = '';
             this.selectedStatus = '';
+            this.startTime = '';
+            this.endTime = '';
         },
         handlePageChange(val) {
-            console.log('page change');
+            console.log('page changed', val);
             this.pageNum = val;
-            this.searchProduct();
+            this.searchReturn();
         },
-        searchProduct() {
-            axios.get('/product/search', {
+        searchReturn() {
+            axios.get('/return/search', {
                 params: {
+                    returnId: this.returnId,
+                    orderId: this.orderId,
+                    customerName: this.customerName,
                     productCode: this.productCode,
                     productName: this.productName,
-                    price: this.price,
-                    stockQuantity: this.stockQuantity,
                     status: this.selectedStatus,
+                    startTimestamp: this.startTime ? this.startTime.getTime() : '',
+                    endTimestamp: this.endTime ? this.endTime.getTime() : '',
                     pageNum: this.pageNum
                 }
             })
@@ -114,5 +127,5 @@ const RoutePage = {
                     console.log(error);
                 });
         }
-    }`
+    }
 }
