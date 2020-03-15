f1169447373d4ed21aa7f92278c8203494f7c970
huangqinkai <2725926259@qq.com>
2020-03-15 19:13:43 +0800

    fix adminustration page

--- dianshang/jcart-administration-front/route-page/router.js
+++ dianshang/jcart-administration-front/route-page/router.js
@@ -5,7 +5,36 @@ const routes = [
     { path: '/customer/search', component: CustomerSearchRoutePage },
     { path: '/order/search', component: OrderSearchRoutePage },
     { path: '/return/search', component: ReturnSearchRoutePage },
-    { path: '/administrator/index', component: AdministratorIndexRoutePage }
+    { path: '/administrator/index', component: AdministratorIndexRoutePage },
+    { path: '/order/search', component: OrderSearchRoutePage },
+
+    { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/customer/show/:customerId', component: CustomerShowRoutePage },
+    { path: '/address/index/:customerId', component: AddressIndexRoutePage },
+    { path: '/address/show/:addressId', component: AddressShowRoutePage },
+
+
+    { path: '/order/search', component: OrderSearchRoutePage },
+    { path: '/order/show/:orderId', component: OrderShowRoutePage },
+
+    { path: '/return/search', component: ReturnSearchRoutePage },
+
+
+    {
+        path: '/return/edit/:returnId',
+        component: ReturnEditRoutePage,
+        children: [
+            {
+                path: 'show',
+                component: ReturnShowRoutePage
+            },
+            {
+                path: 'history',
+                component: ReturnHistoryIndexRoutePage
+            }
+        ]
+    }
+
 ];
 
 const router = new VueRouter({
