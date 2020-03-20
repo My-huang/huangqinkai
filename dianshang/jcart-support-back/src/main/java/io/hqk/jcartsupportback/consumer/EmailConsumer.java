376f8fd2318b599ee6f23dcd3c02b2675d2c9ffc
huangqinkai <2725926259@qq.com>
2020-03-20 20:34:05 +0800

    support rocketmq

---
+++ dianshang/jcart-support-back/src/main/java/io/hqk/jcartsupportback/consumer/EmailConsumer.java
@@ -0,0 +1,36 @@
+package io.hqk.jcartsupportback.consumer;
+
+import io.hqk.jcartsupportback.mp.EmailEvent;
+import io.hqk.jcartsupportback.util.EmailUtil;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RocketMQMessageListener(topic = "SendPwdResetByEmail", consumerGroup = "jcart-support-group01")
+public class EmailConsumer implements RocketMQListener<EmailEvent> {
+
+
+    private Logger logger  = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private EmailUtil emailUtil;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Override
+    public void onMessage(EmailEvent emailEvent) {
+
+
+        logger.info("{}",emailEvent);
+        emailUtil.send(fromEmail,emailEvent.getToEmail(),emailEvent.getTitle(),emailEvent.getContent());
+    }
+
+
+}
